3c086790fe1efa9ebb89a88f1a6024a1946330bf
surajkr20 <suraj71308kumar@gmail.com>
2024-12-15 09:15:17 +0530

    hover effect on resume

--- portfolio/src/components/Resume.jsx
+++ portfolio/src/components/Resume.jsx
@@ -7,8 +7,8 @@ const Resume = ({className=''}) => {
     <div className={`flex flex-row items-center gap-6 ${className}`}>
       {/* Resume section */}
       <a href={'/'} target={"_blank"} 
-      className="bg-white text-black p-2 rounded-md w-[130px] text-center hover:bg-black border-2 border-black hover:text-white
-      dark:border-2 dark:border-light dark:text-light dark:hover:bg-light dark:hover:text-dark">
+      className="bg-white text-black p-2 rounded-md w-[130px] text-center hover:bg-black border-2 border-black
+      dark:border-2 dark:border-light hover:bg-dark hover:text-light dark:text-light dark:hover:bg-light dark:hover:text-dark">
         Resume
       </a>
       <a
