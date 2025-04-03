e06d75d61916e3d1967768bda3fd39fd2a0fec65
surajkr20 <suraj71308kumar@gmail.com>
2025-04-03 08:12:22 +0530

    about page optimized

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -13,34 +13,25 @@ const About = () => {
     <div className=" flex flex-col flex-wrap items-center mx-auto px-8 sm:px-20 sm:py-24 py-20 gap-6 dark:bg-dark dark:text-light">
       {/* introduction section */}
       <div className="flex flex-col justify-between gap-3 shadow-lg dark:border-light dark:border-b-2 p-4 w-full">
-        <div className="flex gap-2 sm:justify-between justify-center  items-center">
-          <h1 className="sm:text-2xl text-3xl">
-            Hello, I&apos;m{" "}
-            <span className="text-blue-600 font-semibold dark:text-primary drop-shadow">
-              Suraj
-            </span>
-          </h1>
-          {/* <div className="h-6 w-1 bg-black"/> */}
+        <div className="flex gap-2 sm:justify-between justify-center items-center">
+          <h2 className="text-red-600 text-xl">
+            <span className="font-serif font-semibold">Hello,</span>
+            i&apos;m Suraj!🙂
+          </h2>
           <h2 className="text-red-600 text-xl sm:block hidden">
             <span className="font-serif font-semibold">Thanks</span> for
             visiting my portfolio!🙂
           </h2>
         </div>
-
-        <p className="sm:text-left text-center text-balance flex sm:flex-row flex-col items-center sm:justify-between justify-center gap-4">
-            <span>
-              I’m a full-stack web developer and a problem solver with
-              aspirations to contribute to projects involving the MERN stack and
-              Java by collaborating with reputed companies in the future.
-              Currently, I am enhancing my DSA skills using Java.
-            </span>
-            <span className="sm:text-left text-center text-balance">
-              I excel in academics and actively participate in various events to
-              gain experience and showcase my skills. Apart from that, I’m a
-              quick learner, a hardworking student, and proficient in English
-              and Hindi.
-            </span>
-          </p>
+        <p className="font-serif hover:scale-105 transition-all">
+          I’m a full-stack web developer and a problem solver with aspirations
+          to contribute to projects involving the MERN stack and Java by
+          collaborating with reputed companies in the future. Currently, I am
+          enhancing my DSA skills using Java.I excel in academics and actively
+          participate in various events to gain experience and showcase my
+          skills. Apart from that, I’m a quick learner, a hardworking student,
+          and proficient in English and Hindi.
+        </p>
       </div>
 
       {/* skills section */}
@@ -48,7 +39,7 @@ const About = () => {
         <h1 className="sm:text-2xl text-3xl font-serif font-semibold text-blue-600 dark:text-primary text-center ">
           My Skills
         </h1>
-        <div className="flex flex-wrap gap-6 justify-between">
+        <div className="flex flex-wrap gap-6 justify-between hover:scale-105 transition-all">
           {skills.map((skill) => (
             <div className="w-14 h-14 flex items-center justify-center box shadow-lg border-r-4 border-b-4 dark:border-light rounded-md">
               <img
@@ -63,24 +54,24 @@ const About = () => {
 
       {/* Eductation section */}
       <div className="py-4 w-full">
-        <div className="flex flex-col gap-3 shadow-lg p-4 dark:border-light dark:border-b-2">
+        <div className="flex flex-col gap-3 shadow-lg p-4 dark:border-light dark:border-b-2 ">
           <h3 className="text-3xl font-semibold font-serif text-blue-600 text-center dark:text-primary">
             Eduction
           </h3>
-          <p className="text-center">
+          <p className="text-center hover:scale-105 transition-all">
             I’ve been on a journey of learning, building a strong foundation in
             technology and problem-solving. Here’s the rundown:
           </p>
         </div>
 
-        <div className="flex text-dark ">
+        <div className="flex text-dark">
           <VerticalTimeline>
             {experiences.map((experience) => (
               <VerticalTimelineElement
                 key={experience.company_name}
                 iconStyle={{ background: experience.iconBg }}
                 icon={
-                  <div className="flex justify-center items-center w-full h-full">
+                  <div className="flex justify-center items-center w-full h-full hover:scale-105 transition-all">
                     <img
                       src={experience.icon}
                       alt={experience.company_name}
