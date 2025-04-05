138d51f446875fed45ccf04eb8721972ecb02a3f
surajkr20 <suraj71308kumar@gmail.com>
2025-04-05 16:28:29 +0530

    deployment error debugging

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/suraj-portfolio" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
