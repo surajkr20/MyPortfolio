2f9fb08657452df1e32f882c4fcfaca888711ab2
surajkr20 <suraj71308kumar@gmail.com>
2025-04-25 14:40:55 +0530

    upated resume

--- portfolio/src/components/Resume.jsx
+++ portfolio/src/components/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = ({className=''}) => {
   return (
     <div className={`flex flex-row mt-4 sm:items-center md:justify-start gap-10 items-center justify-center ${className}` }>
       {/* Resume section */}
-      <a href={'https://drive.google.com/file/d/1lpcLsaxmMl-dzdN-5bVh7yGQQYwuXt7X/view?usp=sharing'} target={"_blank"} 
+      <a href={'https://drive.google.com/file/d/1b9lRM0ko1b_Ib2lo1U0GbX7pca5p7p7R/view?usp=sharing'} target={"_blank"} 
       className="bg-white text-black p-2 rounded-md w-[130px] text-center hover:bg-black border-2 border-black
       dark:border-2 dark:border-light hover:bg-dark hover:text-light dark:text-light dark:hover:bg-light dark:hover:text-dark
       hover:scale-110 transition-all">
